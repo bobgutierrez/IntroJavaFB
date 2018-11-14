9ae2ecd86c7eb740f253b8cc8e85d703ee86d3c3
Simon Roberts <simon@dancingcloudservices.com>
2018-11-14 09:45:31 -0700

    Initial list examples

---
+++ src/usealist/ListStuff.java
@@ -0,0 +1,35 @@
+package usealist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStuff {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<String>();
+
+        System.out.println("Count of items in list is " + names.size());
+
+        names.add("Fred");
+        names.add("Jim");
+        names.add("Sheila");
+        System.out.println(names);
+        System.out.println("Count of items in list is " + names.size());
+
+//        String numberOne = names.get(1);
+//        System.out.println(numberOne);
+
+        System.out.println(names.get(1));
+
+        names.add(0, "William");
+
+        String numberOne = names.get(1);
+        System.out.println(numberOne);
+
+        System.out.println(names);
+
+        int positionOfFred = names.indexOf("Fred");
+        System.out.println("Fred found at position " + positionOfFred);
+        names.add(positionOfFred, "Alice");
+        System.out.println(names);
+    }
+}
